187b7def0756ce93dfadeb65a7f7f40f63b11895
Diego Fornalha <diegofornalha@gmail.com>
2025-01-10 17:38:30 -0300

    ajustado

--- backend-rag-ai/rag_ia/scripts/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-set -a; source .env; set +a; if [ "$ENVIRONMENT" = "production" ]; then echo "Iniciando em modo produção..."; exec gunicorn -c gunicorn.conf.py backend-rag-ai.main:app; else echo "Iniciando em modo desenvolvimento..."; exec uvicorn backend-rag-ai.main:app --host $HOST --port $PORT --reload; fi
