fcd5dffe046851ec91ed6bbd3488b0a80d29e2e9
Diego Fornalha <diegofornalha@gmail.com>
2025-01-10 16:50:43 -0300

    ajustes na nomeclatura

--- backend-rag-ai/__init__.py
+++ backend-rag-ai/__init__.py
@@ -1,5 +1,6 @@
 """
-Backend RAG IA package.
+Backend RAG AI package.
 """
 
 __version__ = "1.0.0"
+name = "backend_rag_ai"
--- backend-rag-ai/setup.py
+++ backend-rag-ai/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="backend-rag-ai",
+    name="backend_rag_ai",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
